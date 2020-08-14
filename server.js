77b7808de3772bd9a69de3ba6bcb3a5e47629678
sobernaut <silwalujjwal@gmail.com>
2020-08-14 23:08:50 +0545

    move server file

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 // Setup static directory to serve
-app.use(express.static(path.resolve('dist')));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve('public', 'index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
