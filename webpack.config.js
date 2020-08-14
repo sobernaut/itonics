496dfedad268a082c3aac913b6472aaef2b2126d
sobernaut <silwalujjwal@gmail.com>
2020-08-14 21:55:37 +0545

    update webpack config to add bundle chunks

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, '/dist'),
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].bundle.js',
+    chunkFilename: '[name].[contenthash].chunk.js',
     publicPath: '/',
   },
   module: {
